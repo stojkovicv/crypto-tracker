36489d15b800b3427f5eaf48bc39a2c595ebd2ce
stepski011 <vukbg.stojkovic@gmail.com>
2023-08-15 20:45:31 +0200

    Message fetching fixed

--- crypto-tracker.js
+++ crypto-tracker.js
@@ -1,4 +1,3 @@
-/*
 // Loading dependencies
 const {Client, Events, GatewayIntentBits} = require('discord.js');
 const { FetchError } = require('./errors');
@@ -14,7 +13,7 @@ import('node-fetch').then(module => {
 // Discord client initializaion
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent]
 });
 
 require('dotenv').config();
@@ -76,62 +75,3 @@ client.on('messageCreate', async message => {
         }
     }
 });
-
-*/
-
-const Discord = require('discord.js');
-const { FetchError } = require('./errors');
-const { FETCH_ERROR } = require('./errorMessages');
-
-const client = new Discord.Client();
-
-require('dotenv').config();
-
-let fetch;
-import('node-fetch').then(module => {
-    fetch = module.default;
-    console.log("node-fetch imported successfully");
-});
-
-async function getBitcoinPrice() {
-    try {
-        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
-        const data = await response.json();
-        console.log("Fetched Bitcoin price:", data.bitcoin.usd);
-        return data.bitcoin.usd;
-    } catch (error) {
-        console.error("Error fetching Bitcoin price:", error);
-        throw new FetchError(FETCH_ERROR);
-    }
-}
-
-async function sendBitcoinPrice(message) {
-    try {
-        const price = await getBitcoinPrice();
-        message.channel.send(`The current price of Bitcoin is $${price}`);
-    } catch (error) {
-        if (error instanceof FetchError) {
-            message.channel.send(error.message);
-        } else {
-            message.channel.send('An unexpected error occurred.');
-        }
-    }
-}
-
-client.on('message', async message => {
-    console.log(`Received message from ${message.author.tag} in ${message.guild ? message.guild.name : 'DM'}: ${message.content}`);
-
-    // Ignore messages from bots
-    if (message.author.bot) return;
-
-    if (message.content === '!bitcoin') {
-        sendBitcoinPrice(message);
-    }
-});
-
-client.once('ready', () => {
-    console.log('Bot is online and running!');
-});
-
-const token = process.env.BOT_TOKEN;
-client.login(token);
